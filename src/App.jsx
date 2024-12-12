232a6eb29deae12911918b6487d6aa9ee73d6013
Mursleen Bukhari <musabukhari20@gmail.com>
2024-12-12 11:47:35 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Footer, Blog , Possibility , WhatGPT3 , Header, Features} from './containers'
+import {Cta,Brand , Navbar } from './components'
+import './app.css'
+
+
+const App = () => {
+  return (
+    
+      <>
+     <div className='App'>
+      <div className='gradient__bg'>
+   <Navbar/>
+   <Header/>
+      </div>
+     <Brand/>
+     <WhatGPT3/>
+     <Features/>
+     <Possibility/>
+     <Cta/>
+     <Blog/>
+     <Footer/>
+     </div>
+      
+      </>
+      
+  )
+}
+
+export default App
